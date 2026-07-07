65ada16e2cd056caa92fa3ff0a6a4279d255a07e
Ajinkya <ajinkyaspatil99@gmail.com>
2026-07-07 20:30:25 +0530

    [closes #00]: website final

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/travel-test-lab/',
+  base: '/travelLab/',
   plugins: [react()],
   resolve: {
     alias: {
